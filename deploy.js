808ba117a30df10034d52934031ca2df999d13da
devwearsprada <borissmeenk@protonmail.com>
2021-07-20 13:21:52 +0200

    :sparkles: Check for matching signatures

--- deploy.js
+++ deploy.js
@@ -1,26 +1,32 @@
 const fs = require('fs')
 const http = require('http')
 const crypto = require('crypto')
-const exec = require('child_process').exec
-
-const secret = '9d0ad87d-952c-41d9-9800-75163ce13c90'
+// const exec = require('child_process').exec
 
 const json = fs.readFileSync('./services.json', 'utf-8')
 const services = JSON.parse(json)
 
 http.createServer((req, res) => {
   req.on('data', (chunk) => {
-    const sig = 'sha1=' +
-      crypto
-        .createHmac('sha1', secret)
-        .update(chunk.toString())
-        .digest('hex')
+    for (const i in services) {
+      const secret = services[i].secret
+      const sig = 'sha1=' +
+        crypto
+          .createHmac('sha1', secret)
+          .update(chunk.toString())
+          .digest('hex')
 
-    console.log(sig)
-    console.log(req.headers)
-    if (req.headers['x-hub-signature'] == sig) {
-      console.log('match')
+      services[i].sha = sig
     }
+
+    const githubSignature = req.headers['x-hub-signature']
+    const match = services.filter((service) => { return service.sha == githubSignature })
+
+    console.log(match.length > 0)
+    console.log(match)
+    // if (req.headers['x-hub-signature'] == sig) {
+    //   console.log('match')
+    // }
   })
 
   res.end()
